ee5bfde297a3d0857b3d6a4f052d6409776d1c8b
Michael Morten <mikemorten1979@gmail.com>
2019-10-06 13:12:25 -0500

    More stability

--- app/controllers/mvpage_controller.rb
+++ app/controllers/mvpage_controller.rb
@@ -6,9 +6,24 @@ class MvpageController < ApplicationController
   end
 
   def moviegrab
+    if params[:movie_breakdown] == "Suspense"
     grabber = MovieDb.find(1)
-    respond_to do |format|
+    elsif params[:movie_breakdown] == "Horror"
+      grabber = MovieDb.find(2)
+    elsif params[:movie_breakdown] == "Love"
+      grabber = MovieDb.find(2)
+    elsif params[:movie_breakdown] == "Mystery"
+      grabber = MovieDb.find(1)
+    elsif params[:movie_breakdown] == "Rated +18"
+      grabber = MovieDb.find(2)
+    elsif params[:movie_breakdown] == "Sci-Fi"
+      grabber = MovieDb.find(3)
+
+  end
+#"Horror","Love", "Mystery","Suspense","Sci-Fi","Rated +18"
+      respond_to do |format|
       format.html { render :movielist, locals: { feedback:params, grabber:grabber }}
     end
+  
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'movie_dbs', to: 'movie_dbs#index', as: 'movie_dbs'
   get 'movie_dbs/:id', to: 'movie_dbs#show', as: 'movie_db'
   get 'movielist/:id', to: 'mvpage#moviegrab', as: 'moviegrabs' 	#display a specific
- # post 'movielist/:id', to: 'mvpage#moviegrab'
+  post 'movielist/:id', to: 'mvpage#moviegrab'
   get 'movielist', to: 'mvpage#moviegrab', as: 'moviegrab'
   post 'movielist/', to: 'mvpage#moviegrab', as: 'movielist'
   get 'movielist', to: 'mvpage#movielist' 

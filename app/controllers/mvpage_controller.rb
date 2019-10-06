68880f6242d7221e7cc4bb316c9c7ae9a99b6a53
Michael Morten <mikemorten1979@gmail.com>
2019-10-06 12:41:58 -0500

    Minor adjustments...

--- app/controllers/mvpage_controller.rb
+++ app/controllers/mvpage_controller.rb
@@ -1,12 +1,12 @@
 class MvpageController < ApplicationController
   def movielist
     respond_to do |format|
-      format.html { render :movielist, locals: { feedback:params } }
+      format.html { render :movielist, locals: { feedback: {}}}
     end
   end
 
   def moviegrab
-    grabber = MovieDb.find(params[:id])
+    grabber = MovieDb.find(1)
     respond_to do |format|
       format.html { render :movielist, locals: { feedback:params, grabber:grabber }}
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'movie_dbs', to: 'movie_dbs#index', as: 'movie_dbs'
   get 'movie_dbs/:id', to: 'movie_dbs#show', as: 'movie_db'
-  get 'movielist/:id', to: 'mvpage#moviegrab', as: 'moviegrab' 	#display a specific 
-  post 'movielist', to: 'mvpage#moviegrab', as: 'moviegrabs'
-  get 'movielist', to: 'mvpage#movielist', as: 'movielist' 
+  get 'movielist/:id', to: 'mvpage#moviegrab', as: 'moviegrabs' 	#display a specific
+ # post 'movielist/:id', to: 'mvpage#moviegrab'
+  get 'movielist', to: 'mvpage#moviegrab', as: 'moviegrab'
+  post 'movielist/', to: 'mvpage#moviegrab', as: 'movielist'
+  get 'movielist', to: 'mvpage#movielist' 
   root to: 'mvpage#movielist'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
